a598d221ce2ac5c3b6799d0b0a3dbc75ee1fc6c2
Prabhjot <pjsingh1359@gmail.com>
2021-09-21 07:11:46 -0700

    add to app and event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: %i[ show edit update destroy ]
-
+  before_action :set_event, only: %i[ show edit update destroy]
+  # before_action :authenticate_user!, except: %i[index show]
   # GET /events or /events.json
   def index
     @events = Event.all
