a1fac943da313fc199828c98bdbb0ef3baaf1375
Prabhjot <pjsingh1359@gmail.com>
2021-09-29 09:06:55 -0700

    work on create and update enrollment commands

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,14 +1,39 @@
 class EnrollmentsController < ApplicationController
 
-  def index
-    @enrollments = Enrollment.all
-  end
-
-  def show
-    @enrollment = Enrollment.find(params[:id])
-  end
-
   def create
-    @enrollment = Enrollment.new(enrollment_params)
+    event = Event.find_by_id(params[:event_id])
+    user = User.find_by_id(params[:user_id])
+    enrollment = Enrollment.new(event_id: event, user_id: user)
+    if enrollment.save
+      flash[:notice] = "You have successfully enrolled in #{event.name}"
+    else
+      flash[:notice] = "You are already enrolled in #{event.name}"
+    end
+    redirect_to event_path(event)
+  end
+
+  def destroy
+    event = Event.find(params[:event_id])
+    enrollment = Enrollment.find(params[:id])
+    if current_user == event.creator
+      enrollment.destroy
+      flash[:notice] = "You have unreserved from the event!"
+    end
+  end
+
+  def update
+    event = Event.find(params[:event_id])
+    @enrollment = Enrollment.find_by(event_id: event.id, user_id: current_user.id)
+    if @enrollment
+      flash[:notice] = "You have successfully updated your reservation!"
+    else
+      flash[:alert] = "You have not updated your reservation!"
+    end
+    redirect_to event_path(event)
+  end
+
+  private
+  def enrollment_params
+    params.require(:enrollment).permit(:event_id, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   resources :users
   resources :enrollments
 
+  resources :events do
+    resources :users
+  end
+
   root to: 'events#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
