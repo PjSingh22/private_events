fb9953c65025857ef9cdc5481b2f7a4139d66cfc
Prabhjot <pjsingh1359@gmail.com>
2021-10-07 07:07:09 -0700

    add validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,10 @@ class Event < ApplicationRecord
   has_many :enrollments, dependent: :destroy
   has_many :attendees, through: :enrollments, source: :user
 
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :body, presence: true, length: { maximum: 200 }
+  validates :date, presence: true
+
 
   scope :past, -> { where('date < ?', Date.today) }
   scope :upcoming, -> { where('date > ?', Date.today) }
